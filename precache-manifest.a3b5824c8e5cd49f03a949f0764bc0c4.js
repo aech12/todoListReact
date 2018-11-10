a55778505edc075e36027dd711877895eb73a170
Alejandro Howez <aech-12@hotmail.com>
2018-11-09 23:39:10 -0300

    Updates

---
+++ precache-manifest.a3b5824c8e5cd49f03a949f0764bc0c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c552b738de9c23266cef",
+    "url": "/todoListReact/static/js/runtime~main.c552b738.js"
+  },
+  {
+    "revision": "fb7f27588728d5a47a87",
+    "url": "/todoListReact/static/js/main.fb7f2758.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/todoListReact/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "fb7f27588728d5a47a87",
+    "url": "/todoListReact/static/css/main.14cbf71b.chunk.css"
+  },
+  {
+    "revision": "79008e22ee14af8dd7885c0eebcc1959",
+    "url": "/todoListReact/index.html"
+  }
+];
